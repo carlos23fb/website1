ef3aecea8125f453fe4613ba4ccc0cfaa3c4d961
jcflobe@gmail.com <jcflobe@gmail.com>
2018-07-08 04:24:26 -0600

    Anadiendo subtitulo a index.php

--- index.php
+++ index.php
@@ -1,5 +1,7 @@
 <?php require('inc/header.php');?>
 <h1>Home</h1>
 <br>
+<h2>Probando Auto deploy a Heroku</h2>
+<br>
 <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Maiores odio unde culpa, illo a harum, voluptates nulla adipisci aspernatur veniam obcaecati fuga consequuntur maxime fugit architecto praesentium! Autem, provident, delectus.</p>
 <?php require('inc/footer.php');?>
